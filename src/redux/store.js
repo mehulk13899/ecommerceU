2c42b60dfe12f0f8ed8a6e3caffe454fd584ea8c
siddharth <opensiddhu993@gmail.com>
2021-03-15 14:37:18 +0530

    remove logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,11 @@ import rootReducer from './root-reducer';
 import logger from "redux-logger";
 
 
-const middleware = [logger]
+const middleware = []
 
-
+if (process.env.NODE_ENV === 'development') {
+    middleware.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middleware));
 
